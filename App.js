bcfecff0460b5672da057953f91b39590aeeed6e
David Thomason <dtom90@users.noreply.github.com>
2018-12-06 17:40:40 -0500

    Added style

--- App.js
+++ App.js
@@ -23,7 +23,7 @@ export default class App extends React.Component {
 
   taskList = (arr) => {
     return arr.map(task =>
-      <Text key={task.id}>{task.name}</Text>
+      <MyText style={styles.task} key={task.id}>{task.name}</MyText>
     )
   };
 
@@ -32,7 +32,7 @@ export default class App extends React.Component {
       <SafeAreaView style={styles.container}>
 
         <View style={styles.headerSection}>
-          <Text>To Do List</Text>
+          <MyText style={{fontSize: 30}}>To Do List</MyText>
           <TextInput
             style={styles.textInput}
             placeholder="enter new task"
@@ -52,24 +52,49 @@ export default class App extends React.Component {
   }
 }
 
+class MyText extends React.Component {
+  render() {
+    const mergedStyles = Object.assign(this.props.style || {}, styles.baseText);
+    return (
+      <Text style={mergedStyles}>{this.props.children}</Text>
+    );
+  }
+}
+
 const styles = StyleSheet.create({
 
   container: {
     flex: 1,
-    backgroundColor: '#fff'
+    backgroundColor: 'white',
+  },
+
+  baseText: {
+    fontFamily: 'Avenir',
   },
 
   headerSection: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
 
-  textInput: {height: 40},
+  titleText: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+
+  textInput: {
+    fontFamily: 'Avenir',
+    height: 40
+  },
 
   tasksSection: {
     flex: 4,
     alignItems: 'center',
+  },
+
+  task: {
+    margin: 10
   }
 
 });
